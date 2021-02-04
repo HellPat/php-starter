43340d17d8635188e64a9085bf3fc55ed8151f63
Patrick Heller <paddy.heller@gmail.com>
2021-02-04 18:13:22 +0100

    shows different actions

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 
 use HellPat\Tools\Http\Response;
 use HellPat\Tools\Http\ServerRequest;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\ErrorHandler\Debug;
 use function Http\Response\send;
 
@@ -14,15 +16,28 @@ $request = ServerRequest::createFromGlobals();
 
 $uri = $request->getUri()->getPath();
 
+$class = new class {
+    public function __invoke(RequestInterface $request): ResponseInterface {
+        return Response::ok('');
+    }
+};
+
 try {
     $action = match($uri) {
         '/' => fn($request) => Response::ok(''),
+        '/response' => Response::ok(''),
+        '/class' => $class,
     };
+
+    if ($action instanceof ResponseInterface) {
+        send($action);
+        return;
+    }
 
     $response = $action($request);
     send($response);
 } catch (UnhandledMatchError $e) {
-    send((fn($request) => Response::notFound(''))($request));
+    send(Response::notFound(''));
 }
 
 
